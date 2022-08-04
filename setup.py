e471d93b052ba693d36395a7620aaf5d57a909d5
Bar Harel <bzvi7919@gmail.com>
2022-08-04 13:32:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="mkdocs-render-swagger-plugin",
-    version="0.0.3",
+    version="0.0.4",
     author="Bar Harel",
     python_requires='>=3.6',
     author_email="bzvi7919@gmail.com",
